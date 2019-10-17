21a7558a768a331612d027614b672cf23d9da20a
Tsatsu Adogla-Bessa Jnr <tsatsua@itconsortiumgh.com>
2019-10-17 01:25:26 +0000

    added working list tags api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='user@company.com', password='password'):
+    """create a sample user """
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
@@ -39,3 +46,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+    def test_tag_str(self):
+        """test tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,65 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse('recipe:tag-list')
+
+
+def sample_user():
+    return get_user_model().objects.create_user(email='user@company.com',
+                                                password='password')
+
+
+class PublicTagsApiTests(TestCase):
+    """test the public tag api's"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required_to_retrieve_tags(self):
+        """test that login is required to retrieve tags"""
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """test the authorized user tag API's"""
+
+    def setUp(self):
+        self.user = sample_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving tags"""
+        Tag.objects.create(user=self.user, name='vegan')
+        Tag.objects.create(user=self.user, name='dessert')
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_authenticated_user(self):
+        """Test that tags are for authenticated user"""
+        user2 = get_user_model().objects.create_user(email='user2@company.com',
+                                                     password='password')
+
+        Tag.objects.create(user=user2, name='fruity')
+        tag = Tag.objects.create(user=self.user, name='comfort food')
+
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
